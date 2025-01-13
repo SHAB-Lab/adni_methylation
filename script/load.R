8dacc0f0084f734b462ef47430ca055ecfde280f
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2025-01-13 15:41:57 -0300

    ignore data, add sample sheet, load data

--- script/load.R
+++ script/load.R
@@ -8,3 +8,19 @@ library("DMRcate")
 library("DMRcatedata")
 library("stringr")
 library("mCSEA")
+
+ann_epic <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
+
+data_dir <- "data/ADNI_iDAT_files"
+list.files(data_dir, recursive = TRUE)
+
+# READ DATA
+targets <- read.metharray.sheet("data", pattern = "Sample_Sheet.csv")
+# READ ONLY SMALL SET
+tar_small <- targets[1:10,]
+rg_set <- read.metharray.exp(targets = tar_small)
+
+# SUMMARIZE DATA
+rg_set
+pData(rg_set)
+getManifest(rg_set)
