6f74a58e2d572935cc056252b60f4aefb03f158a
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2025-02-07 16:44:47 -0300

    update script

--- script/load.R
+++ script/load.R
@@ -18,9 +18,55 @@ list.files(data_dir, recursive = TRUE)
 targets <- read.metharray.sheet("data", pattern = "Sample_Sheet.csv")
 # READ ONLY SMALL SET
 tar_small <- targets[1:10, ]
-rg_set <- read.metharray.exp(targets = tar_small)
+rgSet <- read.metharray.exp(targets = tar_small)
 
 # SUMMARIZE DATA
-rg_set
-pData(rg_set)
-getManifest(rg_set)
+rgSet
+pData(rgSet)
+getManifest(rgSet)
+
+# Para transformar los ensayos rojos y verdes en señales metiladas 
+# y no metiladas correspondientemente se utilizará el siguiente código:
+MSet <- preprocessRaw(rgSet) 
+MSet 
+# Comparar con rgSet 
+
+# Si corremos getMeth y getUnmeth podemos ver las intensidades de 
+# las matrices metiladas y no metiladas.   
+
+head(getMeth(MSet)[,1:3]) 
+head(getUnmeth(MSet)[,1:3]) 
+
+# Crear un RatioSet con ratioConvert 
+ratioSet <- ratioConvert(MSet, what = "both", keepCN = TRUE)
+# Observar los cambios en el ensayo
+ratioSet
+
+gset <- mapToGenome(ratioSet)
+gset
+
+# Las funciones getBeta, getM y getCN permiten obtener los valores Beta, 
+# M y el número de copias de la matrix 
+
+beta <- getBeta(gset) 
+head(beta) 
+m <- getM(gset) 
+head(m) 
+cn <- getCN(gset) 
+head(cn) 
+
+#Control de Calidad 1
+qc <- getQC(MSet) 
+plotQC(qc) 
+
+# Para calcular el valor de p
+detP <- detectionP(rgSet) 
+head(detP) 
+
+# Resumir los valores de p en un puto único para simplificar
+# la comparación entre muestras
+# Examinar el promedio de los valores de p en todas las muestras
+# para identificar cualquier muestra fallida
+par(mar=c(9, 6, 4, 2))
+barplot(colMeans(detP), las=2, cex.names=0.8)
+mtext("Mean detection p-values", 2, 5)
